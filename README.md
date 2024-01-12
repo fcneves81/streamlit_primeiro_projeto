44b095a2358e2e54326567fabbe71d4c82fab2c4
Fernando Camargo <fcneves81@gmail.com>
2024-01-11 23:32:07 -0300

    Primeiro projeto com Streamlit

---
+++ README.md
@@ -0,0 +1,35 @@
+﻿
+# **MEU PRIMEIRO PROJETO UTILIZANDO O STREAMLIT**
+
+## Conhecendo o Streamlit
+
+Streamlit é uma biblioteca de código aberto para Python que permite criar aplicativos web interativos usando apenas código Python. Com o Streamlit, você pode transformar seus scripts Python em aplicativos web interativos de forma rápida e fácil, sem a necessidade de conhecimentos avançados em desenvolvimento web. O Streamlit oferece uma ampla gama de recursos e widgets que permitem personalizar e aprimorar a aparência e a funcionalidade dos seus aplicativos. Além disso, o Streamlit permite que você atualize e visualize instantaneamente as alterações em seu aplicativo, facilitando o processo de desenvolvimento e depuração.
+
+### *Linguagem utilizada:*
+
+![Python](https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white) <br />
+
+
+### *Bibiliotecas utilizadas:*
+
+Uma listagem das bibliotecas utilizadas no projeto.
+A lista completa está no arquivo <code>requirements.txt</code>
+
+✔️ Requests <br>
+✔️ Streamlit <br>
+✔️ Streamlit_lottie <br>
+✔️ Pillow
+
+### *Funcionalidade do aplicativo:*
+
+<p> Este é meu primeiro projeto utilizando o Streamlit.
+</p>
+<p>
+A ideia principal é criar uma pequena apresentação sobre mim e fornecer uma forma de contato através do formulário para envio de mensagem (sim, o formulário funciona! Porque você não testa e me manda uma mensagem dizendo o que achou e o que posso melhorar?).
+</p>
+<p>
+Espero cada dia melhorar mais e aprender novas funcionalidades.</p>
+<p>Espero ainda mais, que você tenha gostado do que viu aqui.</p>
+<p>
+Se gostou, deixe uma estrela no projeto para me incentivar e fique a vontade para me seguir!
+</p>  
---
+++ app.py
@@ -0,0 +1,101 @@
+﻿import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+from PIL import Image
+
+st.set_page_config(page_title="Fernando Camargo", page_icon=":tada:", layout="wide")
+
+def load_lottie(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# Meu CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</styel>", unsafe_allow_html=True)
+
+local_css("style/style.css")
+
+# Assets
+
+lottie_animation = load_lottie("https://lottie.host/52a3d559-0f5c-446a-a75d-662a7cfa85ca/kfVf1sJ66u.json")
+banner = Image.open("images/capa-github.png")
+
+# HEADER
+
+with st.container():
+    st.subheader("Que bom ter você aqui 😉")
+    st.image(banner)
+    st.title("Engenheiro de Sotfware em formação")
+    st.write("A cada dia, aprendendo mais sobre o maravilhoso mundo do Python e Java para enfrentar os desafios do dia a dia!")
+    st.write("[Conheça meus projetos e aprendizado até aqui >](https://github.com/fcneves81)") 
+ 
+# SOBRE MIM
+
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("Minhas Competências")
+        st.write("##")
+        st.write(
+            """
+            HARD SKILLS:
+            - Java
+            - Python
+                - Pandas
+                - Seaborn
+                - Django
+                - Streamlit (que inclusive, está sendo usado neste projeto 🤩)
+            - HTML, CSS e JavaScript
+            - BootStrap
+            - Banco de Dados Relacionais: MySQL, PostgreSQL e SQL Server
+            - Banco de Dados não Relacionais: MongoDB
+            - Git
+            - Power BI
+            
+            SOFT SKILLS:
+            - Ética
+            - Empatia
+            - Flexibilidade
+            - Inteligência Emocional
+            - Sou colaborativo
+            - Organizado
+            - Resiliente
+            - Sei trabalhar sob pressão
+            - Bom relacionamento interpessoal
+            - Criatividade            
+            
+            """
+        )
+    with right_column:
+        st_lottie(lottie_animation, height=800, key="coding")
+
+# --- Contato
+with st.container():
+    st.write("---")
+    st.header("Entre em contato comigo!")
+    st.write("##")
+    
+    # Formulário utilizado: https://formsubmit.co/
+    
+    form_contato = """
+    <form action="https://formsubmit.co/fcneves81@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Seu nome" required>
+        <input type="email" name="email" placeholder="Seu email" required>
+        <textarea name="message" placeholder="Sua mensagem" required></textarea>
+        <button type="submit">Enviar</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)    
+    with left_column:
+        st.markdown(form_contato, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
+    
+    
+    
+
